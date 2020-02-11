69f872d86c53f0d822e50e2c8cbfa46a2550a0cb
Gabriel <gabrielciortea@gmail.com>
2020-02-11 17:42:58 +0000

    implemented entity framework component + basic webform

---
+++ HBSWeb/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HBSDatabase;
+
+namespace HBSWeb
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (HBSModel _entity = new HBSModel())
+            {
+                var _users = _entity.Users.ToList();
+                Console.WriteLine(_users[0].Username);
+            }
+        }
+    }
+}
